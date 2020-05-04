d552d9c93dcf7a39f90418825f28bd3488af5d56
Scott Schindler <scottschindler29@gmail.com>
2020-05-04 17:40:24 -0400

    mvp

--- adv.py
+++ adv.py
@@ -30,8 +30,51 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+reverse_directions = {'n':'s', 's':'n','e':'w','w':'e'}
 
-# TRAVERSAL TEST - DO NOT MODIFY
+reverse_path = []
+
+visited = {}
+
+
+#Let's start with current room and get the exits from that room 
+visited[player.current_room.id] = player.current_room.get_exits()
+
+# Let us traverse the room while the number of rooms we have visited < total num of rooms
+while len(visited) < len(room_graph) -1:
+	#if the current room is not in the dictionary set of visited rooms
+	if player.current_room.id not in visited:
+		 # Let's add the room to the visited dictionary, and keep track of what exits are there, value is lists
+		visited[player.current_room.id] = player.current_room.get_exits()
+
+        # Remove the exit one at a time so we do not traverse the way we came immediately
+		visited[player.current_room.id].remove(reverse_path[-1]) 
+	
+	
+    # While the room has no more exits  and we hit a deadend 
+	while len(visited[player.current_room.id]) == 0:
+		# Get the last direction (the way we came in)
+		reverse = reverse_path.pop()
+		# update traversal Path (travel back that way)
+		traversal_path.append(reverse)
+		#update player movement in the opposite direction as we need to back track to a room where above condition is not met
+		player.travel(reverse)
+	
+	# Go to the first available exit (last added to the list for that room entry in the dictionary) 
+	movement = visited[player.current_room.id].pop()
+
+	# Update the path 
+	traversal_path.append(movement)
+
+	# Update the reverse path (for back tracking)
+	reverse_path.append(reverse_directions[movement])
+
+	# Update player movement to go in that direction
+	player.travel(movement)
+
+
+
+# TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
